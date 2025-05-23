b30bfb7eac69083ae9a58d571461ee01620ed4c6
dir-FY25jioshimizu <jio.shimizu.f317@dirbato.co.jp>
2025-05-23 16:24:57 +0900

    rennsyuu

---
+++ sample/src/sample/Compare.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Compare {
+
+   
+        // TODO 自動生成されたメソッド・スタブ
+        public static void main(String[] args) { 
+            var num = 2; 
+            while (num <= 1024) { 
+            System.out.println(num); 
+            num = num * 2;
+            }
+        }
+}
+
+
+
--- sample/src/sample/variable.java
+++ sample/src/sample/variable.java
@@ -11,10 +11,21 @@ public class variable {
         System.out.println(str1);
         
         String str2  = "kudo nozomi";
-        System.out.println(str2);
+        System.out.println(str1+str2);
+        
+        int mybirthday = 20000926;
+        System.out.println(mybirthday);
+        
+        int a = 7;
+        int b = 77;
+        int c = a*b;
+        System.out.println(c);
+        
+        float a1 = 10.1f;
+        int d = 100;
+        System.out.println(a1*d);
         
         
-
     }
 
 }
