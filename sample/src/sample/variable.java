d8da31c65e5bf64de31df0473e2d37eff99768cb
dir-FY25jioshimizu <jio.shimizu.f317@dirbato.co.jp>
2025-05-23 15:19:36 +0900

    チャプター３．２

---
+++ sample/src/sample/variable.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class variable {
+
+    public static void main(String[] args) {
+        var number1 = 2;
+        System.out.println(number1);
+        
+        String str1;
+        str1 = "the great";
+        System.out.println(str1);
+        
+        String str2  = "kudo nozomi";
+        System.out.println(str2);
+        
+        
+
+    }
+
+}
